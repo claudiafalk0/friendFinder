bc2370e49796cc6f5824e5726f5dd4ef21f60b40
Claudia Falk <claudiafalk0@gmail.com>
2019-07-08 20:47:19 -0600

    added name tags

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+var friends = [
+
+    {
+        name: 'Monica Geller',
+        photo: monica_geller.jpg,
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+        
+    }
+]
