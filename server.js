c0f01b8343f1b49fd2211accebf1d31d04c68034
Claudia Falk <claudiafalk0@gmail.com>
2019-07-09 19:19:03 -0600

    updated server.js

--- server.js
+++ server.js
@@ -4,23 +4,28 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-var friends = [
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+app.listen(PORT, function(){
+    console.log('App listening on PORT: ' + PORT);
+})
+// var friends = [
 
-    {
-        name: 'Monica Geller',
-        photo: monica_geller.jpg,
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
+//     {
+//         name: 'Monica Geller',
+//         photo: monica_geller.jpg,
+//         scores: [
+//             5,
+//             1,
+//             4,
+//             4,
+//             5,
+//             1,
+//             2,
+//             5,
+//             4,
+//             1
+//         ]
         
-    }
-]
+//     }
+// ]
