532e3bb91ed2b84498e241d83127d498847f9c6d
Claudia Falk <claudiafalk0@gmail.com>
2019-07-10 21:04:33 -0600

    fixed math to get total of recent push

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,20 @@ module.exports = function(app){
     });
 
     app.post('/api/friends', function(req,res){
+        var total = 0;
         tableData.push(req.body);
-
+        res.json(true);
+        for(i=0; i < req.body.score.length; i++){
+            var num = parseInt(req.body.score[i]);
+            console.log(num);
+            total = total + num;
+        }
+        console.log(total)
     });
+
+    // app.post('/api/friends', function(req,res){
+    //     // for(i=0; i < tableData.length; i++){
+    //     //     if()
+    //     // }
+    // })
 };
