5c8c5e1bf303dd37d74b261beac316d881f130f5
Claudia Falk <claudiafalk0@gmail.com>
2019-07-11 22:34:32 -0600

    fixed modal, img still not working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,19 @@
 var tableData = require('../data/friends');
+var total = 0;
+var winningTotal = 0;
+var winningObj = 0;
+var totalArr = [];
 
 module.exports = function(app){
-    app.get('/api/friends', function(req,res){
-        res.json(tableData);
-    });
-
     app.post('/api/friends', function(req,res){
-        var total = 0;
-        var winningTotal = 0;
-        var winningObj = 0;
-        var totalArr = [];
         tableData.push(req.body);
         res.json(true);
-        for(i=0; i < req.body.scores.length; i++){
-            var num = parseInt(req.body.scores[i]);
-            total += num;
-        }
-        console.log(total);
+
+            for(i=0; i < req.body.scores.length; i++){
+                var num = parseInt(req.body.scores[i]);
+                total += num;
+            }
+            console.log(total);
         for(i = 0; i < (tableData.length - 1); i++){
             var scoreArr = tableData[i].scores;
             totalArr[i] = 0;
@@ -27,7 +24,7 @@ module.exports = function(app){
             }
         }
         for(i = 0; i < totalArr.length; i++) {
-        // totalArr.forEach(function(val){
+            // totalArr.forEach(function(val){
             var x, y = 0;
             totalArr[i] > total ? x = totalArr[i] : y = totalArr[i];
             totalArr[i] < total ? x = total : y = total; 
@@ -40,12 +37,15 @@ module.exports = function(app){
         // console.log('This is the winning Arr ' + totalArr[i]);
         console.log(totalArr[0] + "|" + totalArr[1] + "|" + winningTotal);
         console.log(tableData[winningObj]);
-        module.exports(tableData[winningObj]);
+        // res.send({friendsData: tableData[winningObj]});
         // console.log('arrays total: ' + arrTotal);
+    });
+    app.get('/api/friends', function(req,res){
+        res.json({friendsData:tableData[winningObj]});
     });
 
     // app.post('/api/friends', function(req,res){
-    //     // for(i=0; i < tableData.length; i++){
+        //     // for(i=0; i < tableData.length; i++){
     //     //     if()
     //     // }
     // })
